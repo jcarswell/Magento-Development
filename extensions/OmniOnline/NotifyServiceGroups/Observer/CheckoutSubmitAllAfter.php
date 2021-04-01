e43ceedff15d2380b536f44053c87a6c64a0cf30
Josh Carswell <jcarswell@evrazplace.com>
2021-04-01 22:38:21 +0000

    Fixed Observer checkout handler to not die when an item doesn't have a notification configuration

--- extensions/OmniOnline/NotifyServiceGroups/Observer/CheckoutSubmitAllAfter.php
+++ extensions/OmniOnline/NotifyServiceGroups/Observer/CheckoutSubmitAllAfter.php
@@ -14,26 +14,6 @@ class CheckoutSubmitAllAfter implements ObserverInterface
 
     public function execute(\Magento\Framework\Event\Observer $observer) {
 
-        $qcx_parade_email = $this->scopeConfig->getValue(
-            'service_groups_notification_settings/email_addresses/qcx_parade_email',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-
-        $qcx_exhibit_email = $this->scopeConfig->getValue(
-            'service_groups_notification_settings/email_addresses/qcx_exhibitor_email',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-
-        $it_email = $this->scopeConfig->getValue(
-            'service_groups_notification_settings/email_addresses/it_services_email',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-
-        $elec_email = $this->scopeConfig->getValue(
-            'service_groups_notification_settings/email_addresses/electrical_services_email',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-
         $store_email = $this->scopeConfig->getValue(
             'trans_email/ident_sales/email',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
@@ -43,7 +23,7 @@ class CheckoutSubmitAllAfter implements ObserverInterface
     
         $billingAddress = $order->getBillingAddress();
         $clientInfo = '<h2>Order ID: ' . $order->getIncrementId() . "</h2>\n<h2>Customer</h2>\n<p>";
-        $clientInfo .= $billingAddress->getName() . "<br>\n" . $billingAddress->getCompany() . "<br>\n" . $order->getCustomerEmail() . "<br>\n";
+        $clientInfo .= $billingAddress->getName() . "<br>\n" . $billingAddress->getCompany() . "<br>\n" . $order->getCustomerEmail() . "<br>\n". $billingAddress->getTelephone() . "<br>\n";
         foreach ($billingAddress->getStreet() as $streetAddress) {
             if ($streetAddress) {
                 $clientInfo .= $streetAddress . "<br>\n";
@@ -52,64 +32,85 @@ class CheckoutSubmitAllAfter implements ObserverInterface
         $clientInfo .= $billingAddress->getRegion() . ', ' . $billingAddress->getCountryId() . "<br>\n" . $billingAddress->getPostcode() . "</p>\n<h2>Requested Items</h2>\n";
 
         $items = $order->getAllItems();
-        $orderInfo = array('it' => '', 'elec' => '', 'qcx_parade' => '', 'qcx_exhibit' => '');
+        $orderInfo = array();
         foreach ($items as $item) {
-            $itemInfo = '<table><tr><td colspan="2"><strong>' . $item->getName() . "</strong></td></tr>\n";
-            $data = $item->getData();
-            $itemInfo .= '<tr><td><strong>Quantity:</strong> ' . $data['qty_ordered'] . '</td>';
-            $i = 0;
-            $specialInstructions = '';
-            foreach ($data['product_options']['options'] as $option) {
-                if ($option['label'] == 'Special Instructions') {
-                    $specialInstructions = $option['print_value'];
-                } else {
-                    if ($i % 2 == 1) {
-                        $itemInfo .= "</tr><tr>\n";
+            try {
+                $notifyIds = explode(',',$item->getProduct()->getData('notify_service_group'));
+                if (!empty($notifyIds)) {
+                    $itemInfo = '<table><tr><td colspan="2"><strong>' . $item->getName() . "</strong></td></tr>\n";
+                    $data = $item->getData();
+                    $itemInfo .= '<tr><td><strong>Quantity:</strong> ' . $data['qty_ordered'] . '</td>';
+                    $i = 0;
+                    $specialInstructions = '';
+                    foreach ($data['product_options']['options'] as $option) {
+                        if ($option['label'] == 'Special Instructions') {
+                            $specialInstructions = $option['print_value'];
+                        } else {
+                            if ($i % 2 == 1) {
+                                $itemInfo .= "</tr><tr>\n";
+                            }
+                            $itemInfo .= '<td><strong>' . $option['label'] . ':</strong> ' . $option['print_value'] . '</td>';
+                            $i++;
+                        }
                     }
-                    $itemInfo .= '<td><strong>' . $option['label'] . ':</strong> ' . $option['print_value'] . '</td>';
-                    $i++;
+                    if ($i % 2 == 1) {
+                        $itemInfo .= '<td>&nbsp;</td>';
+                    }
+                    if ($specialInstructions) {
+                            $itemInfo .= "</tr>\n<tr><td colspan=\"2\"><strong>Special Instructions:</strong> " . $specialInstructions . '</td>';
+                    }
+                    $itemInfo .= "</tr></table>\n<br>\n";
+                    foreach ($notifyIds as $notifyId) {
+                        $notifyGroup = $item->getProduct()->getResource()->getAttribute('notify_service_group')->getSource()->getOptionText($notifyId);
+                        if (!empty($notifyGroup)) {
+                            if (!isset($orderInfo[$notifyGroup])) {
+                                $orderInfo[$notifyGroup] = "";
+                            $orderInfo[$notifyGroup] .= $itemInfo;
+                            }
+                        }
+                    }
                 }
-            }
-            if ($i % 2 == 1) {
-                $itemInfo .= '<td>&nbsp;</td>';
-            }
-            if ($specialInstructions) {
-                    $itemInfo .= "</tr>\n<tr><td colspan=\"2\"><strong>Special Instructions:</strong> " . $specialInstructions . '</td>';
-            }
-            $itemInfo .= "</tr></table>\n<br>\n";
-            $notifyIds = explode(',',$item->getProduct()->getData('notify_service_group'));
-            foreach ($notifyIds as $notifyId) {
-                $notifyGroup = $item->getProduct()->getResource()->getAttribute('notify_service_group')->getSource()->getOptionText($notifyId);
-                if ($notifyGroup == 'IT Services') {
-                    $orderInfo['it'] .= $itemInfo;
-                } elseif ($notifyGroup == 'Electrical Services') {
-                    $orderInfo['elec'] .= $itemInfo;
-                } elseif ($notifyGroup == 'QCX Parade') {
-                    $orderInfo['qcx_parade'] .= $itemInfo;
-                } elseif ($notifyGroup == 'QCX Exhibitor') {
-                    $orderInfo['qcx_exhibit'] .= $itemInfo;
-                }
+            } 
+            catch (Exception $e) {
+                $notifyIds = NULL;
             }
         }
 
         $headers  = 'From: ' . $store_email . "\r\n";
         $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-        if (!empty($orderInfo['it'])) {
-            $body = "<h1>IT Services Request</h1>\n" . $clientInfo . $orderInfo['it'];
-            mail($it_email, 'There is a new IT Services request', $body, $headers);
+        if (!empty($orderInfo['IT Services'])) {
+            $email = $this->scopeConfig->getValue(
+                'service_groups_notification_settings/email_addresses/it_services_email',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+            $body = "<h1>IT Services Request</h1>\n" . $clientInfo . $orderInfo['IT Services'];
+            mail($email, 'There is a new IT Services request', $body, $headers);
         }
-        if (!empty($orderInfo['elec'])) {
-            $body = "<h1>Electrical Services Request</h1>\n" . $clientInfo . $orderInfo['elec'];
-            mail($elec_email, 'There is a new Electrical Services request', $body, $headers);
+        if (!empty($orderInfo['Electrical Services'])) {
+            $email = $this->scopeConfig->getValue(
+                'service_groups_notification_settings/email_addresses/electrical_services_email',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+            $body = "<h1>Electrical Services Request</h1>\n" . $clientInfo . $orderInfo['Electrical Services'];
+            mail($email, 'There is a new Electrical Services request', $body, $headers);
         }
-        if (!empty($orderInfo['qcx_parade'])) {
-            $body = "<h1>QCX Parade Application</h1>\n" . $clientInfo . $orderInfo['qcx_parade'];
-            mail($elec_email, 'There is a new QCX Parade Application', $body, $headers);
+        if (!empty($orderInfo['QCX Parade'])) {
+            $email = $this->scopeConfig->getValue(
+                'service_groups_notification_settings/email_addresses/qcx_parade_email',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+            $body = "<h1>REAL Kids Summer Camp Registration</h1>\n" . $clientInfo . $orderInfo['QCX Parade'];
+            mail($email, 'There is a new REAL Kids Summer Camp Registration', $body, $headers);
         }
-        if (!empty($orderInfo['qcx_exhibit'])) {
-            $body = "<h1>New Order</h1>\n" . $clientInfo . $orderInfo['qcx_exhibit'];
-            mail($elec_email, 'There is a new order', $body, $headers);
+        if (!empty($orderInfo['QCX Exhibitor'])) {
+            $email = $this->scopeConfig->getValue(
+                'service_groups_notification_settings/email_addresses/qcx_exhibitor_email',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+            $body = "<h1>New Order</h1>\n" . $clientInfo . $orderInfo['QCX Exhibitor'];
+            mail($email, 'There is a new order', $body, $headers);
         }
     }
 }
+
