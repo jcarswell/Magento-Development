3a3f490394722534f102219df77827caf75771c9
Josh Carswell <JCarswell@evrazplace.com>
2021-04-07 12:17:16 -0600

    Forgot to save...doh!

--- extensions/REAL/Bootstrap4/registration.php
+++ extensions/REAL/Bootstrap4/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'REAL_CheckoutSidebar',
+	'REAL_Bootstrap4',
 	__DIR__
 );
