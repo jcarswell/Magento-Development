10874b64772bf77da3d27801b78f083429a0b89f
Josh Carswell <JCarswell@evrazplace.com>
2021-04-08 13:38:23 -0600

    Update block referance and added to settings

---
+++ extensions/REAL/CheckoutSidebar/Model/ConfigProvider.php
@@ -0,0 +1,25 @@
+<?php 
+namespace REAL\CheckoutSidebar\Model;
+ 
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Cms\Block\Widget\Block;
+ 
+class ConfigProvider implements ConfigProviderInterface
+{
+   /** @var LayoutInterface  */
+   protected $cmsBlockWidget;
+ 
+   public function __construct(Block $block, $blockId)
+   {
+    $this->cmsBlockWidget = $block;
+    $block->setData('block_id', $blockId);
+    $block->setTemplate('Magento_Cms::widget/static_block/default.phtml');
+    }
+
+    public function getConfig()
+    {
+        return [
+            'cms_block' => $this->cmsBlockWidget->toHtml()
+        ];
+    }
+}
