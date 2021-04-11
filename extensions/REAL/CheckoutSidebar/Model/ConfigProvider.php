14251a1d05cdaeb14624d148b0a2024e6e7f55e1
Josh Carswell <JCarswell@evrazplace.com>
2021-04-11 12:29:38 -0600

    Checkout block should now render based on the value configured

--- extensions/REAL/CheckoutSidebar/Model/ConfigProvider.php
+++ extensions/REAL/CheckoutSidebar/Model/ConfigProvider.php
@@ -2,24 +2,64 @@
 namespace REAL\CheckoutSidebar\Model;
  
 use Magento\Checkout\Model\ConfigProviderInterface;
-use Magento\Cms\Block\Widget\Block;
+use Magento\Store\Model\ScopeInterface;
  
 class ConfigProvider implements ConfigProviderInterface
 {
-   /** @var LayoutInterface  */
-   protected $cmsBlockWidget;
- 
-   public function __construct(Block $block, $blockId)
-   {
-    $this->cmsBlockWidget = $block;
-    $block->setData('block_id', $blockId);
-    $block->setTemplate('Magento_Cms::widget/static_block/default.phtml');
+    /** 
+     * Path to the config node
+     */
+    const PATH_CMS_BLOCK = 'checkout/cms_block_config/checkout_cms_block';
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfiguration;
+
+    /** 
+     * @var LayoutInterface
+     */
+    protected $cmsBlockWidget;
+
+    /** 
+     * Init
+     * 
+     * @param \Magento\Cms\Block\Widget\Block $block
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        \Magento\Cms\Block\Widget\Block $block,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        )
+    {
+        $this->scopeConfiguration = $scopeConfig;
+        $this->cmsBlockWidget = $block;
     }
 
+    /**
+     * return the block Config data
+     * 
+     * @return array
+     */
     public function getConfig()
     {
+        $block->setData('block_id', $this->getBlockConfig());
+        $block->setTemplate('Magento_Cms::widget/static_block/default.phtml');
         return [
             'cms_block' => $this->cmsBlockWidget->toHtml()
         ];
+    }
+
+    /**
+     * Returns blockId
+     * 
+     * @return int
+     */
+    public function getBlockConfig()
+    {
+        return $this->scopeConfiguration->getValue(
+            self::PATH_CMS_BLOCK,
+            ScopeInterface::SCOPE_STORE
+        );
     }
 }
