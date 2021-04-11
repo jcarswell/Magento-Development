6c0448e8629c13a251d62b2db49ced7094f89979
Josh Carswell <JCarswell@evrazplace.com>
2021-04-11 16:34:16 -0600

    troubleshooting

--- extensions/REAL/Bootstrap4/view/frontend/requirejs-config.js
+++ extensions/REAL/Bootstrap4/view/frontend/requirejs-config.js
@@ -1,11 +1,11 @@
 var config = {
     map: {
         '*': {
-            bootstrap4: 'REAL_Bootstrap4/js/bootstrap'
+            'bootstrap4': 'REAL_Bootstrap4/js/bootstrap.bundle'
         }
     },
     shim: {
-        bootstrap4: {
+        'bootstrap4': {
             'deps': ['jquery']
         }
     }
